2b2d6652048b51baea11768f5154d8a0180d28e9
Manuel Tello Siri <manuelTello.business@gmail.com>
2022-05-07 14:44:33 -0300

    Se altero la estructura de los componentes para un uso adaptativo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,25 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import {NavLink, Link} from "react-router-dom";
 import {Nav} from "react-bootstrap";
 import {buttonActive} from "../lib/button.js";
 
 const {Item} = Nav;
 
 const NavBar = ()=>{
-    return <Nav as={"ul"}>
-        <Item as={"li"}>
-            <NavLink style={buttonActive} to="/">Home</NavLink>
-        </Item>
-        <Item as={"li"}>
-            <NavLink style={buttonActive} to="/categorys">Categorias</NavLink>
-        </Item>
-        <Item as={"li"}>
-            <NavLink style={buttonActive} to="/topsells">Top sellers</NavLink>
-        </Item > 
-        <Item as={"li"}>
-            <NavLink style={buttonActive} to="/auth/login">Log in</NavLink>
-        </Item>
-        <Item as={"li"}>
-            <NavLink style={buttonActive} to="/auth/signin">Sign in</NavLink>
-        </Item>
-        <Item as={"li"}>
-            <NavLink style={buttonActive} to="/auth/profile">Profile</NavLink>
-        </Item>
-    </Nav>
+    return <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+        <div className="container">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <NavLink className={"nav-link"} aria-current="page" to={"/"}>Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className={"nav-link"} to={"/categorys"}>Categorys</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+  </nav>
 };
 
 export default NavBar;
--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import useFetch from "../hooks/useFetch.js";
-//import lista from "../assets/categorys.json";
 
 const Categorys = ()=>{
     const lista = useFetch("https://honeysuckle-giant-ambert.glitch.me/category/all");
-    return <div>
-        {lista && lista.data.map((cat)=>{
+
+    return <div className="container py-1 my-5 bg-secondary bg-opacity-25 rounded">
+        {lista && lista.map((cat)=>{
             const {name,subcat} = cat;
-            return <div key={name}> 
-                <h1><Link to={`/categorys/${name}`}>{name}</Link></h1>
-                <ul>{subcat.map(e =><li key={e}><Link to={`/categorys/${name}/${e}`}>{e}</Link></li>)}</ul>
+            return <div key={name} className="m-4"> 
+                <div className="row">
+                    <div className="col-12 text-center text-sm-start">
+                        <h4>
+                            <Link key={name} to={`/categorys/${name}`} style={{textDecoration:"none"}}>{name}</Link>
+                        </h4>
+                    </div>
+                    {subcat.map(e =><div className={"col-12 col-sm-3 text-center text-sm-start"}>
+                        <Link key={e} to={`/categorys/${name}/${e}`} style={{textDecoration:"none"}}>
+                            <h7 className="text-secondary">{e}</h7>
+                        </Link>
+                    </div>)}
+                </div>
             </div>
         })}
     </div>
