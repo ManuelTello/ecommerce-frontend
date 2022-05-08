65229fb0c5149f5ffb4280472810375ed0a26836
ManuelTello <93638568+ManuelTello@users.noreply.github.com>
2022-05-08 15:02:16 -0300

    Menores cambios en la barra de navegacion para un contenido mas adaptable

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,38 +1,23 @@
-import React from "react";
-import {NavLink} from "react-router-dom";
+import NavBarLink from "../parts/navbar/NavBarLinks.jsx";
+import NavBarLogo from "../parts/navbar/NavBarLogo.jsx";
+import NavBarSearch from "../parts/navbar/NavBarSeach.jsx";
 
 const NavBar = ()=>{
-    return <nav className="navbar navbar-expand navbar-dark bg-primary">
+    return (
+    <nav className="navbar navbar-expand navbar-dark bg-primary">
         <div className="container">
             <div className="row align-items-center">
-                <div className="col-12 col-sm-4 navbar-brand">
-                    <h2 className="text-center">SuperMarket</h2>
-                </div>
+                <NavBarLogo/>
                 <div className="col">
                     <div className="row">
-                        <div className="col">
-                            <form method="GET" action="#" className="d-flex">
-                                  <input type="text" className="form-control me-2" placeholder="Buscar"/>
-                                  <button type="submit" className="btn mx-2 btn-primary btn-outline-light d-none d-sm-inline-block">Buscar</button>
-                            </form>
-                        </div>
-                        <div className="">
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                    <li className="nav-item"><NavLink className={"nav-link"} aria-current="page" to={"/"}>Home</NavLink></li>
-                                    <li className="nav-item"><NavLink className={"nav-link"} aria-current="page" to={"/categorys"}>Categorias</NavLink></li>
-                                    <li className="nav-item"><NavLink className={"nav-link"} aria-current="page" to={"/profile/historial"}>Mis compras</NavLink></li>
-                                    <li className="nav-item"><NavLink className={"nav-link"} aria-current="page" to={"/auth/login"}>Log In</NavLink></li>
-                                    <li className="nav-item"><NavLink className={"nav-link"} aria-current="page" to={"/auth/signin"}>Sign In</NavLink></li>
-                                    <li className="nav-item"><NavLink className={"nav-link"} aria-current="page" to={"/profile"}>Perfil</NavLink></li>
-                                </ul>
-                            </div>
-                        </div>
+                        <NavBarSearch/>     
+                        <NavBarLink/>
                     </div>
                 </div>
             </div>
         </div>
-  </nav>
+    </nav>
+    );
 };
 
 export default NavBar;
