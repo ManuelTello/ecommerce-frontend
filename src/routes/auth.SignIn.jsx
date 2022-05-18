e22eebaf4a3bb2b07f52d7966c6d2e4a1cf8d90d
ManuelTello <93638568+ManuelTello@users.noreply.github.com>
2022-05-18 16:14:46 -0300

    Se agrego los campos de inscripcion y se modoficaron los componentes para ser adaptativos

--- src/routes/auth.SignIn.jsx
+++ src/routes/auth.SignIn.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const AuthSignin = ()=>{
-    return <h1>Sign in page</h1>
+    return <div className="container-md my-5 p-5 rounded-1 bg-opacity-25 bg-secondary" >
+        <form action="#" method="POST">
+            <div className="row justify-content-center">
+                <div class="mb-3 col-12">
+                    <label for="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp"/>
+                </div>
+                <div class="mb-3 col-6">
+                    <label for="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password"/>
+                </div>
+                <div class="mb-3 col-6">
+                    <label for="password-rep" className="form-label">Repeat password</label>
+                    <input type="password" className="form-control" id="password-rep"/>
+                </div>
+
+
+                <div className="col-5 mb-3">
+                    <label for="name" className="form-label">Name</label>
+                    <input type="text" className="form-control"></input>
+                </div>
+                <div className="col-5 mb-3">
+                    <label for="" className="form-label">Last name</label>
+                    <input type="text" className="form-control"></input>
+                </div>
+                <div className="col-2 mb-3">
+                    <label for="" className="form-label">Age</label>
+                    <input type="number" className="form-control"></input>
+                </div>
+
+                <div className="col">
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </div>
+       </form>
+    </div>
 };
 
 export default AuthSignin;
